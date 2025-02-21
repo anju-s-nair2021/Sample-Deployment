ddbc0314682e357d607c19f85515be422ec22dee
Anju <anjusreeni2021@gmail.com>
2025-02-20 21:11:31 -0800

    static modified

--- mypro/mypro/settings.py
+++ mypro/mypro/settings.py
@@ -120,6 +120,7 @@ import os
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles') 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 
 MEDIA_ROOT=os.path.join(BASE_DIR,"MEDIA")
 MEDIA_URL='/media/'
