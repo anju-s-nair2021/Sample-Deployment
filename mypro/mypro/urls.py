146bf67f2274f3d4c0bc2f3c72dd58bd923b5b9b
Anju <anjusreeni2021@gmail.com>
2025-02-24 02:28:37 -0800

    url  modified

--- mypro/mypro/urls.py
+++ mypro/mypro/urls.py
@@ -36,4 +36,9 @@ urlpatterns = [
     path('index/',myapp.views.index,name='index'),
 
 
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
+# Serve media files
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
